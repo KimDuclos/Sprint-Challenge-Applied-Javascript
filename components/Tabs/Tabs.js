83377dc89ed8023a6f497738f1b87d354ae119e4
Kim Duclos <kimduclos@gmail.com>
2019-02-01 09:46:37 -0700

    finished TabCard section and Start Here (which explains why things where working before)

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -57,11 +57,11 @@ class TabLink {
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+    this.cardElement = cardElement;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-    // this.cardElement;
+    this.cardElement.display = 'flex';
   }
 
 }
@@ -70,9 +70,12 @@ class TabCard {
 
 - Select all classes named ".tab" and assign that value to the tabs variable
 
-- With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
+- With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM 
+NodeList
 
-- In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
+- In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a 
+parameter
 
 */
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll('.tab');
+tabs.forEach(tab => new TabLink(tab));
